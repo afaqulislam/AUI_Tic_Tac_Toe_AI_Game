ebf10d9dd55b681f57237c05392edb0ab30425d6
Afaq Ul Islam <afaqulislam707@gmail.com>
2025-06-25 22:14:46 +0500

    AUI Tic Tac Toe AI Game

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AUI TIC TAC TOE AI GAME',
+  description: 'Tic-Tac-Toe game with AI and multiplayer modes, built using Next.js and Radix UI on the frontend, with a Python-powered backend. Created by AUI.',
+  generator: 'NEXTJS',
 }
 
 export default function RootLayout({
